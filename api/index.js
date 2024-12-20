23d295d3a497f834872763017f311693322f1f9b
Melrain <yezyd92@icloud.com>
2024-12-20 22:16:42 +0800

    1

--- api/index.js
+++ api/index.js
@@ -1,9 +1,9 @@
-import { createServer } from 'http';
-import { Server } from 'socket.io';
+import { createServer } from "http";
+import { Server } from "socket.io";
 
 const requestHandler = (req, res) => {
   res.writeHead(200);
-  res.end('Server is running');
+  res.end("Server is running");
 };
 
 const httpsServer = createServer(requestHandler);
@@ -11,26 +11,31 @@ const httpsServer = createServer(requestHandler);
 const io = new Server(httpsServer, {
   cors: {
     origin:
-      process.env.NODE_ENV === 'production'
+      process.env.NODE_ENV === "production"
         ? false
-        : ['https://localhost:5500'],
+        : ["https://localhost:5500"],
   },
 });
 
-io.on('connection', (socket) => {
+io.on("connection", (socket) => {
   console.log(`User ${socket.id} connected`);
 
-  socket.on('message', (data) => {
+  socket.on("message", (data) => {
     console.log(data);
-    io.emit('message', `${socket.id.substring(0, 5)}: ${data}`);
+    io.emit("message", `${socket.id.substring(0, 5)}: ${data}`);
   });
 
-  socket.on('disconnect', () => {
+  const intervalId = setInterval(() => {
+    console.log("send message");
+    socket.emit("message", `Server: ${new Date().toISOString()}`);
+  }, 1000);
+
+  socket.on("disconnect", () => {
     console.log(`User ${socket.id} disconnected`);
     clearInterval(intervalId); // 清除定时器
   });
 });
 
 httpsServer.listen(3500, () => {
-  console.log('listening on port 3500');
+  console.log("listening on port 3500");
 });
